509dffdf8d5ffc2865158565f3d6efb4f9655834
Emma <contact@emma.pizza>
2025-04-23 17:14:25 +0200

    update tests (doesn't mean they all pass... :( )

--- tests/default.nix
+++ tests/default.nix
@@ -37,6 +37,15 @@ in runNixOSTest {
         waitForInternet = false;
       };
     };
+    nothing = { config, pkgs, ... }: {
+      services.flatpak = {
+        enable = true;
+        # equivalent to not setting them, but I figured I'd make it clear to my future self
+        remotes = {};
+        packages = [];
+        waitForInternet = false;
+      };
+    };
     installation = { config, lib, pkgs, ... }: {
       services.flatpak = {
         enable = true;
@@ -69,34 +78,25 @@ in runNixOSTest {
   };
 
   testScript = ''
-    start_all()
-
     disabled.wait_for_unit("multi-user.target")
     disabled.succeed("which flatpak")
     disabled.fail("systemctl status --no-pager manage-system-flatpaks.service")
-    disabled.shutdown()
   
     bare.wait_for_unit("multi-user.target")
     bare.succeed("which flatpak")
     bare.succeed("systemctl list-unit-files -l | grep 'manage-system-flatpaks'")
-    bare.shutdown()
 
-    custom_dirs.wait_for_unit("multi-user.target")
-    custom_dirs.wait_for_unit("manage-system-flatpaks.service")
+    nothing.wait_for_unit("manage-system-flatpaks.service")
+
     custom_dirs.wait_until_succeeds("stat /target", timeout=60)
-    custom_dirs.shutdown()
 
-    installation.wait_for_unit("multi-user.target")
-    installation.wait_for_unit("manage-system-flatpaks.service")
     installation.wait_until_succeeds("stat /target/.module", timeout=120)
     installation.wait_until_succeeds("stat /target/repo", timeout=120)
     installation.wait_until_succeeds("stat /target/exports", timeout=120)
     installation.succeed("stat /target/exports/bin/org.kde.xwaylandvideobridge")
     installation.succeed("flatpak run --command=true org.kde.xwaylandvideobridge")
-    installation.fail("flatpak run --command=false org.kde.xwaylandvideobridge")
   
     persist.start(allow_reboot=True)
-    persist.wait_for_unit("multi-user.target")
     persist.wait_for_unit("manage-system-flatpaks.service")
     persist.wait_for_file("/target/repo", timeout=120)
     # Added by POST hook, both should succeed
@@ -104,12 +104,10 @@ in runNixOSTest {
     persist.succeed("stat /target/thisfileshouldnotpersist")
     persist.reboot()
     # errors begin here...
-    persist.wait_for_unit("multi-user.target")
     persist.wait_for_unit("manage-system-flatpaks.service")
     persist.wait_until_succeeds("stat /target/.module/new", timeout=60)
     persist.wait_until_fails("stat /target/.module/new", timeout=60)
     persist.succeed("stat /target/repo/thisfileshouldpersist")
     persist.fail("stat /target/thisfileshouldnotpersist")
-    persist.shutdown()
   '';
 }
