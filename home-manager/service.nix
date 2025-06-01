50d5c662bcd1fa5c323932c807fb390f6f5207b8
Emma <contact@emma.pizza>
2025-06-01 16:26:09 +0200

    update service definitions

--- home-manager/service.nix
+++ home-manager/service.nix
@@ -1,45 +1,48 @@
 { config, lib, ... }:
 
 let
-  inherit (lib) mkIf;
-  
+  inherit (lib) mkIf pipe recursiveUpdate;
   cfg = config.services.flatpak;
+  applyUnitOrdering = prev: recursiveUpdate {
+    Install.WantedBy = [
+      "default.target"
+    ];
+  } prev;
+  applyServiceRestart = prev: recursiveUpdate {
+    Unit = {
+      StartLimitIntervalSec = 60;
+      RefuseManualStart = true;
+      StartLimitBurst = 3;
+    };
+    Service = {
+      Restart = "on-failure";
+    };
+  } prev;
 in 
 
 {
-  config.systemd.user = mkIf cfg.enable {
-    services."manage-flatpaks" = {
-      Unit = {
-        Wants = mkIf cfg.runOnActivation [
-          "network.target"
-        ];
-        After = mkIf cfg.runOnActivation [
-          "network.target"
-        ];
+  config.systemd.user = {
+    services."manage-flatpaks-activation" = pipe {
+      Unit = rec {
+        Conflicts = "manage-flatpaks-auto.service";
+        Description = "Manage flatpaks";
+        Before = Conflicts;
       };
-      Install.WantedBy = mkIf cfg.runOnActivation [
-        "default.target"
-      ];
-      Service.ExecStart = config.services.flatpak.mainScript;
-    };
-    timers = mkIf (cfg.onCalendar != null) {
-      "manage-flatpaks" = {
-        Unit = {
-          Wants = [
-            "network.target"
-          ];
-          After = [
-            "network.target"
-          ];
-        };
-        Install.WantedBy = [
-          "default.target"
-        ];
-        Timer = {
-          OnCalendar = cfg.onCalendar;
-          Persistent = true;
-        };
+      Service.ExecStart = config.services.flatpak.mainScript.activation;
+    } [ applyUnitOrdering applyServiceRestart (mkIf cfg.enable) ];
+    services."manage-flatpaks-auto" = pipe {
+      Unit = rec {
+        Conflicts = "manage-flatpaks-activation.service";
+        Description = "Manage flatpaks";
+        After = Conflicts;
       };
-    };
+      Service.ExecStart = config.services.flatpak.mainScript.auto;
+    } [ applyServiceRestart (mkIf cfg.enable) ];
+    timers."manage-flatpaks-auto" = pipe {
+      Timer = {
+        OnCalendar = cfg.onCalendar;
+        Persistent = true;
+      };
+    } [ applyUnitOrdering (mkIf cfg.enable) ];
   };
 }
--- nixos/service.nix
+++ nixos/service.nix
@@ -1,39 +1,66 @@
 { config, lib, ... }:
 
 let
-  inherit (lib) mkIf optionals;
-  
+  inherit (lib) mkIf pipe recursiveUpdate;
   cfg = config.services.flatpak;
+  applyUnitOrdering = prev: recursiveUpdate {
+    unitConfig = rec {
+      Wants = After;
+      After = [
+        "network-online.target"
+        "nss-lookup.target"
+      ];
+    };
+    wantedBy = [
+      "multi-user.target"
+    ];
+  } prev;
+  applyServiceRestart = prev: recursiveUpdate {
+    unitConfig = {
+      StartLimitIntervalSec = 60;
+      RefuseManualStart = true;
+      StartLimitBurst = 3;
+    };
+    serviceConfig = {
+      Restart = "on-failure";
+    };
+  } prev;
+  # unsure if this causes issues
+  # applyServiceSandboxing = prev: recursiveUpdate {
+  #   serviceConfig = {
+  #     RequiresMountsFor = [ cfg.flatpakDir ];
+  #     ReadWritePaths = [ cfg.flatpakDir ];
+  #     ProtectSystem = "strict";
+  #     PrivateDevices = true;
+  #     ProtectHome = true;
+  #     PrivateTmp = true;
+  #   };
+  # } prev;
 in
 
 {
-  config.systemd = mkIf cfg.enable {
-    services."manage-flatpaks" = {
-      description = "Manage flatpaks";
-      wants = mkIf cfg.runOnActivation [
-        "network-online.target"
-      ];
-      after = mkIf cfg.runOnActivation [
-        "network-online.target"
-        (mkIf cfg.waitForInternet "nss-lookup.target")
-      ];
-      wantedBy = mkIf cfg.runOnActivation [
-        "multi-user.target"
-      ];
-      serviceConfig.ExecStart = config.services.flatpak.mainScript;
-    };
-    timers."manage-flatpaks" = mkIf (cfg.onCalendar != null) {
-      wants = [
-        "network-online.target"
-      ];
-      after = [
-        "network-online.target"
-      ] ++ (optionals cfg.waitForInternet [ "nss-lookup.target" ]);
-      wantedBy = [ "multi-user.target" ];
+  config.systemd = {
+    services."manage-flatpaks-activation" = pipe {
+      unitConfig = rec {
+        Conflicts = "manage-flatpaks-auto.service";
+        Description = "Manage flatpaks";
+        Before = Conflicts;
+      };
+      serviceConfig.ExecStart = config.services.flatpak.mainScript.activation;
+    } [ applyUnitOrdering applyServiceRestart (mkIf cfg.enable) ];
+    services."manage-flatpaks-auto" = pipe {
+      unitConfig = rec {
+        Conflicts = "manage-flatpaks-activation.service";
+        Description = "Manage flatpaks";
+        After = Conflicts;
+      };
+      serviceConfig.ExecStart = config.services.flatpak.mainScript.auto;
+    } [ applyServiceRestart (mkIf cfg.enable) ];
+    timers."manage-flatpaks-auto" = pipe {
       timerConfig = {
         OnCalendar = cfg.onCalendar;
         Persistent = true;
       };
-    };
+    } [ applyUnitOrdering (mkIf cfg.enable) ];
   };
 }
