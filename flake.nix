9a9a3f15218e91246589c6e6177a33accaffe218
Emma <contact@emma.pizza>
2025-06-02 15:08:04 +0200

    remove deprecated flake outputs

--- flake.nix
+++ flake.nix
@@ -2,8 +2,5 @@
   outputs = { self }: {
     nixosModule = ./nixos;
     homeModule = ./home-manager;
-
-    nixosModules.declarative-flatpak = builtins.trace "declarative-flatpak: this output (nixosModules.declarative-flatpak) will be removed soon, please use output nixosModule" self.nixosModule;
-    homeManagerModules.declarative-flatpak = builtins.trace "declarative-flatpak: this output (homeManagerModules.declarative-flatpak) will be removed soon, please use output homeModule" self.homeModule;
   };
 }
