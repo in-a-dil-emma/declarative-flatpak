3c0b0ab4c57108e9b746eb48b154809f448457e6
musjj <72612857+musjj@users.noreply.github.com>
2025-09-24 00:01:06 +0700

    add default flake modules

--- flake.nix
+++ flake.nix
@@ -2,7 +2,15 @@
   outputs =
     { self }:
     {
-      nixosModule = ./nixos;
-      homeModule = ./home-manager;
+      nixosModule = builtins.warn "declarative-flatpak: flake output `nixosModule` has been renamed to `nixosModules`" ./nixos;
+      homeModule = builtins.warn "declarative-flatpak: flake output `homeModule` has been renamed to `homeModules`" ./home-manager;
+      nixosModules = {
+        default = self.outputs.nixosModules.declarative-flatpak;
+        declarative-flatpak = ./nixos;
+      };
+      homeModules = {
+        default = self.outputs.homeModules.declarative-flatpak;
+        declarative-flatpak = ./home-manager;
+      };
     };
 }
