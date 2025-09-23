0d066bf21415400b3651445a85b8c0b5bfda2e80
Emma <contact@emma.pizza>
2025-09-23 09:46:11 +0200

    generate ini for new override option

--- module/internal-options.nix
+++ module/internal-options.nix
@@ -1,13 +1,23 @@
-{ config, lib, ... }:
+{ config, pkgs, lib, ... }:
 
 let
-  inherit (lib) mkOption;
+  ini-types = callPackage ../lib/types/ini.nix {};
+
+  inherit (lib) mkOption mapAttrs pipe;
+  inherit (pkgs) callPackage;
+  inherit (ini-types) ini;
 
   cfg = config.services.flatpak;
   fallback = x: y: if x == null then y else x;
 in
 {
   options.services.flatpak.internal = {
+    overrideFiles = mkOption {
+      internal = true;
+      default = pipe config.services.flatpak.overrides [
+        (mapAttrs (name: ini.generate "flatpak-override-${name}"))
+      ];
+    };
     targetDir = mkOption {
       internal = true;
       apply = fallback cfg.flatpakDir;
