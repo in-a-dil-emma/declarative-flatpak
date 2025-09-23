eb9b9b8fbd3272d5d1a42bae3303e2b39798cef0
Emma <contact@emma.pizza>
2025-09-23 09:59:41 +0200

    add treefmt

--- shell.nix
+++ shell.nix
@@ -1,9 +1,12 @@
 let
   inputs = import ./npins;
   pkgs = import inputs.nixpkgs {};
+  treefmt = import inputs.treefmt-nix;
+
   inherit (pkgs) mkShellNoCC npins ncurses ostree gawk jq;
 in mkShellNoCC {
   packages = [
+    (treefmt.mkWrapper pkgs ./lib/treefmt.nix)
     ncurses
     ostree
     npins
