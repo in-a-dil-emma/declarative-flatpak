f74b247745b68fd1ffa3ea83fae9ac23bb93e957
Emma <contact@emma.pizza>
2025-09-26 11:12:47 +0200

    github action
    ignore the force push :)

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-git fetch --tags --force --prune-tags
-git tag -d latest
-git tag "$@" # intentional choice; if no parameters are given, just show the tags
-_latest=$(git tag | sort -rV | head -n1)
-git tag latest -m 'latest release' "$_latest"^{}
-git push --tags --force
