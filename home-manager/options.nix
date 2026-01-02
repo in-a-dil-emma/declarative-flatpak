c5f4b309ce501639a660bed8c5a991c65c385c0d
Emma <contact@emma.pizza>
2026-01-02 12:15:26 +0100

    home-manager module: add assertion for hm+nixos setups, implementing warning in README
    closes #57

--- home-manager/options.nix
+++ home-manager/options.nix
@@ -1,12 +1,27 @@
-{ config, lib, ... }@args:
+{ config
+, lib
+, nixosConfig ? {}
+, ... }:
 let
-  inherit (lib) mkOption;
+  inherit (lib) mkOption mkIf;
   inherit (lib.types) bool;
 in
 {
   options.services.flatpak.enable = mkOption {
     type = bool;
-    default = args.nixosConfig.services.flatpak.enable or false;
+    default = nixosConfig.services.flatpak.enable or false;
   };
-  config.services.flatpak.internal.targetDir = "${config.xdg.dataHome}/flatpak";
+  config = {
+    services.flatpak.internal.targetDir = "${config.xdg.dataHome}/flatpak";
+    assertions = mkIf (nixosConfig ? services.flatpak.enable) [ {
+      assertion = config.services.flatpak.enable && nixosConfig.services.flatpak.enable;
+      message = ''
+        You're using home-manager with NixOS.
+        Flatpak is not enabled in your NixOS config.
+        This setup is unsupported.
+
+        https://github.com/in-a-dil-emma/declarative-flatpak/issues/57#issuecomment-3705068763
+      '';
+    } ];
+  };
 }
