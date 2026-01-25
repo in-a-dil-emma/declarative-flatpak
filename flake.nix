630cb0ce9de3ce63e254bf46eb7990db7c9005de
Hana Kretzer <144328493+nanoyaki@users.noreply.github.com>
2026-01-25 21:48:53 +0100

    fix: default nixos/home module outputs
    Fixes an evaluation error when trying to import the module using the default output

--- flake.nix
+++ flake.nix
@@ -5,11 +5,11 @@
       nixosModule = builtins.warn "declarative-flatpak: flake output `nixosModule` has been renamed to `nixosModules.default`" ./nixos;
       homeModule = builtins.warn "declarative-flatpak: flake output `homeModule` has been renamed to `homeModules.default`" ./home-manager;
       nixosModules = {
-        default = self.outputs.nixosModules.declarative-flatpak;
+        default = self.nixosModules.declarative-flatpak;
         declarative-flatpak = ./nixos;
       };
       homeModules = {
-        default = self.outputs.homeModules.declarative-flatpak;
+        default = self.homeModules.declarative-flatpak;
         declarative-flatpak = ./home-manager;
       };
     };
