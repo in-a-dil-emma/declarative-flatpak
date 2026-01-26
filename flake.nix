05a9b67e643124ad2acfcde000ef089ce71eeb4b
Emma <contact@emma.pizza>
2026-01-26 12:41:14 +0100

    flake: remove deprecated outputs

--- flake.nix
+++ flake.nix
@@ -2,8 +2,6 @@
   outputs =
     { self }:
     {
-      nixosModule = builtins.warn "declarative-flatpak: flake output `nixosModule` has been renamed to `nixosModules.default`" ./nixos;
-      homeModule = builtins.warn "declarative-flatpak: flake output `homeModule` has been renamed to `homeModules.default`" ./home-manager;
       nixosModules = {
         default = self.nixosModules.declarative-flatpak;
         declarative-flatpak = ./nixos;
