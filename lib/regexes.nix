cc26aad694732ce3cb1b22673aa1dd8d47abaff2
amanako <208144045+amanako@users.noreply.github.com>
2026-09-17 14:40:35 +0200

    remove ill-defined escapes from Nix regex strings

--- lib/regexes.nix
+++ lib/regexes.nix
@@ -4,9 +4,9 @@ rec {
   farch = "[0-9x_a-zA-Z-]*";
   fbranch = "[a-zA-Z0-9.-]+";
   fcommit = "[a-z0-9]{64}";
-  ffile = "\.flatpak(ref)?";
+  ffile = ".flatpak(ref)?";
 
-  fpkgnet = "${fremote}:${ftype}\/${fref}\/${farch}\/${fbranch}(:${fcommit})?";
+  fpkgnet = "${fremote}:${ftype}/${fref}/${farch}/${fbranch}(:${fcommit})?";
   fpkglocal = "(${fremote})?:.+${ffile}";
 
   fremote = "[A-Za-z0-9-]+";
