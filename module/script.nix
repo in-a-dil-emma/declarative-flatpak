85c3656236ee78458e0a2809bd22859aabeea93d
Emma <contact@emma.pizza>
2026-09-22 11:58:06 +0200

    fmt script 1/2

--- module/script.nix
+++ module/script.nix
@@ -60,10 +60,13 @@ let
       ;
   });
   system-user-switch = if is-hm then "--user" else "--system";
-  script = mapAttrs (name: value: ''
-    echo "Executing step: ${name}"
-    ${value}
-  '') {
+  script =
+    mapAttrs
+      (name: value: ''
+        echo "Executing step: ${name}"
+        ${value}
+      '')
+  {
     config-diff = optionalString (!cfg.alwaysRunOnActivation) ''
       if [ -e "$DATA_DIR/config" ] && cmp -s ${filecfg} "$DATA_DIR/config"; then
         echo "Configs do not differ, therefore I won't do anything. You may change this default behaviour"
